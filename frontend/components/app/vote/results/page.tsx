1a42fa0adbc01a7fd0c0e7d33996bdf57b3f0881
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-02-18 06:26:03 +0530

    Create page.tsx

---
+++ frontend/components/app/vote/results/page.tsx
@@ -0,0 +1,13 @@
+import LiveResults from "@/components/LiveResults";
+
+export default function ResultsPage() {
+  return (
+    <div className="p-10 max-w-xl mx-auto">
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        
+      </h1>
+
+      <LiveResults />
+    </div>
+  );
+}
